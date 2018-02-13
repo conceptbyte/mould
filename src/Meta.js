daa6dc2ff8e0f8b3ee527fa1908a8149b5ba0d50
Rajdeep Tarat <rajdeeptarat@gmail.com>
2018-02-13 15:44:12 +0530

    Meta object can parse the data and return meta objects.

--- src/Meta.js
+++ src/Meta.js
@@ -1,15 +1,28 @@
 class Meta {
 
+    /**
+     * Construct an instance of class
+    */
     constructor() {
         this.key = 'meta';
+        this.items = {};
     }
 
+    /**
+     * Returns the string representation of class
+    */
     toString() {
         return this.key;
     }
 
+    /**
+     * Parse the meta data passed in
+     *
+     * @param {Object} items
+     */
     parse(items) {
-        return items;
+        this.items = items;
+        return this.items;
     }
 
 }
--- tests/Meta.test.js
+++ tests/Meta.test.js
@@ -5,3 +5,11 @@ test('It creates a meta object', (t) => {
     let meta = new Meta;
     t.truthy(String(meta) == 'meta');
 });
+
+test('It parses meta data objects', (t) => {
+    t.plan(1);
+
+    let meta = new Meta;
+    meta = meta.parse({ one:1, two: 2 });
+    t.deepEqual(meta, { one: 1, two: 2 });
+});
